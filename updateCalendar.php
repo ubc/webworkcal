2f373a307e0ffe4aadac105838282f305837ef22
Compass <pan.luo@ubc.ca>
2013-02-05 14:16:59 -0800

    fix typo

--- updateCalendar.php
+++ updateCalendar.php
@@ -37,7 +37,7 @@ $client->setUseObjects(true);
 /* Load the key in PKCS 12 format - remember: this is the file you had to
  * download when you created the Service account on the API console.
  */
-if (dirname($config['key_file'] == '.') {
+if (dirname($config['key_file']) == '.') {
     // expend to full path
     $config['key_file'] = dirname(__FILE__).'/'.$config['key_file'];
 }
